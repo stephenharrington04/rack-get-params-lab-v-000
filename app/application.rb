fd8f2c7b8206b0f35bbcfe5137b9b06eb87f4b1d
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-30 16:13:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      item_to_add = req.params["q"]
+      item_to_add = req.params["item"]
       if @@items.include?(item_to_add)
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
